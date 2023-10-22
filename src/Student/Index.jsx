4ba410b248102c5f84a37b36e65e87845ecba613
Aaryan87 <102862016+Aaryan87@users.noreply.github.com>
2023-10-22 17:43:38 +0530

    Redux Toolkit

---
+++ src/Student/Index.jsx
@@ -0,0 +1,15 @@
+import { Provider } from 'react-redux';
+import { store } from './store';
+import StudentForm from './StudentForm';
+import StudentList from './StudentList';
+
+const Index = () => {
+  return (
+    <Provider store={store}>
+        <StudentForm />
+        <StudentList />
+    </Provider>
+  )
+}
+
+export default Index;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Router from './Router/Router.jsx'
+import Index from './Student/Index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router/>
+    {/* <Router/> */}
+    <Index/>
   </React.StrictMode>,
 )
