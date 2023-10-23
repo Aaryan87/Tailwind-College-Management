65191ef308d69a4f5ba2b29aa5f68bfb0f973048
Aaryan87 <102862016+Aaryan87@users.noreply.github.com>
2023-10-24 00:15:52 +0530

    A

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -9,8 +9,6 @@ import ContactUs from "../Last-Pages/ContactUs"
 import Student_App from "../Student-Dash-Board/Student_App"
 import Student_Main from "../Student-Dash-Board/Student_Main"
 import Calendar from "../calendar/cn_output"
-import Staff_Index from "../Staff/Staff_Index"
-import Staff_Filter_Index from "../Staff/Staff_Filter_Index"
 import Student_Filter_Index from "../Student/Student_Filter_Index"
 
 const Router = () => {
@@ -24,12 +22,10 @@ const Router = () => {
                     {/* Admin dashboard Routing done here */}
                     <Route path="/" element={<App />}>
                         <Route index element={<Main />} />
-                        
+
                         <Route path="addstudent" element={<Index/>} />
                         <Route path="/filter-student" element={<Student_Filter_Index/>}/>
 
-                        <Route path="addstaff" element={<Staff_Index/>}/>
-                        <Route path="/filter-staff" element={<Staff_Filter_Index/>} />
                         <Route path="assisgnment" element={<FileUpload/>} />
                         <Route path="contactus" element={<ContactUs/>} />
                     </Route>
