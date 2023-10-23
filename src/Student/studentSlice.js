0225abb40a4fdf008ffb627572d421258da9c03c
Aaryan87 <102862016+Aaryan87@users.noreply.github.com>
2023-10-23 23:50:59 +0530

    A

--- src/Student/studentSlice.js
+++ src/Student/studentSlice.js
@@ -1,19 +1,47 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Load state from local storage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('students');
+    if (serializedState === null) {
+      return [];
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error('Get state error: ', err);
+    return [];
+  }
+};
+
+// Save state to local storage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('students', serializedState);
+  } catch (err) {
+    console.error('Set state error: ', err);
+  }
+};
+
 const studentSlice = createSlice({
   name: 'students',
-  initialState: [],
+  initialState: loadState(),
   reducers: {
     addStudent: (state, action) => {
       state.push(action.payload);
+      saveState(state);
     },
     deleteStudent: (state, action) => {
-      return state.filter(student => student.id !== action.payload);
+      const newState = state.filter(student => student.id !== action.payload);
+      saveState(newState);
+      return newState;
     },
     updateStudent: (state, action) => {
       const index = state.findIndex(student => student.id === action.payload.id);
       if (index !== -1) {
         state[index] = action.payload;
+        saveState(state);
       }
     }
   }
