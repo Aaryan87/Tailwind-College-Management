f796a5954cda10b054a0efaf89c23c1aea115d3b
Aaryan87 <102862016+Aaryan87@users.noreply.github.com>
2023-10-23 18:07:44 +0530

    A

--- src/Dash-Board/Main.jsx
+++ src/Dash-Board/Main.jsx
@@ -17,18 +17,15 @@ const Main = () => {
 
       {/* First Card Component */}
 
+      <Link to="addstudent">
       <div className=' h-[100px] rounded-[8px] bg-white border-l-[4px] border-[#4E73DF] flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out'>
         <div>
           <h2 className='text-[#B589DF] text-[11px] leading-[17px] font-bold'>Add Student</h2>
           <h1 className='text-[20px] leading-[24px] font-bold text-[#5a5c69] mt-[5px]'>CRUD</h1>
         </div>
-        <Link to="addstudent">
-          <button>
-            <FaRegCalendarMinus fontSize={28} color="" />
-          </button>
-        </Link>
-        
+          <FaRegCalendarMinus fontSize={28} color="" /> 
       </div>
+      </Link>
 
         {/* Second Card Component */}
 
