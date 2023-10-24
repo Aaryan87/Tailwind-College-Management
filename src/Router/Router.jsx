aa1dc70411e9d0e3428969a941798acea33b2fe4
Abhishek <abhitiku2003@gmail.com>
2023-10-24 15:37:00 +0530

    added tailwind typography and dark version for login page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,53 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import App from '../App'
-import Main from "../Dash-Board/Main"
-import Index from "../Student/Index"
-import FileUpload from "../File-Upload/FileUpload"
-import Login from "../Starting-Pages/Login"
-import Register from "../Starting-Pages/Register"
-import ContactUs from "../Last-Pages/ContactUs"
-import Student_App from "../Student-Dash-Board/Student_App"
-import Student_Main from "../Student-Dash-Board/Student_Main"
-import Calendar from "../calendar/cn_output"
-import Student_Filter_Index from "../Student/Student_Filter_Index"
-import Index1 from "../Staff/Index1"
-import Staff_Filter_Index from "../Staff/Staff_Filter_Index"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+import Main from "../Dash-Board/Main";
+import Index from "../Student/Index";
+import FileUpload from "../File-Upload/FileUpload";
+import Login from "../Starting-Pages/Login";
+import Register from "../Starting-Pages/Register";
+import ContactUs from "../Last-Pages/ContactUs";
+import Student_App from "../Student-Dash-Board/Student_App";
+import Student_Main from "../Student-Dash-Board/Student_Main";
+import Calendar from "../calendar/cn_output";
+import Student_Filter_Index from "../Student/Student_Filter_Index";
+import Index1 from "../Staff/Index1";
+import Staff_Filter_Index from "../Staff/Staff_Filter_Index";
 
 const Router = () => {
   return (
     <div>
-        <BrowserRouter>
-                <Routes>
-                    <Route path="/register" element={<Register/>} />
-                    <Route path="/login" element={<Login/>} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
-                    {/* Admin dashboard Routing done here */}
-                    <Route path="/" element={<App />}>
-                        <Route index element={<Main />} />
+          {/* Admin dashboard Routing done here */}
+          <Route path="/" element={<App />}>
+            <Route index element={<Main />} />
 
-                        <Route path="addstudent" element={<Index/>} />
-                        <Route path="/filter-student" element={<Student_Filter_Index/>}/>
+            <Route path="addstudent" element={<Index />} />
+            <Route path="/filter-student" element={<Student_Filter_Index />} />
 
-                        <Route path="addstaff" element={<Index1/>}/>
-                        <Route path="/filter-staff" element={<Staff_Filter_Index/>}/>
+            <Route path="addstaff" element={<Index1 />} />
+            <Route path="/filter-staff" element={<Staff_Filter_Index />} />
 
-                        <Route path="assisgnment" element={<FileUpload/>} />
-                        <Route path="contactus" element={<ContactUs/>} />
-                        <Route path="calendar" element={<Calendar/>}/>
-                    </Route>
+            <Route path="assisgnment" element={<FileUpload />} />
+            <Route path="contactus" element={<ContactUs />} />
+            <Route path="calendar" element={<Calendar />} />
+          </Route>
 
-                    {/* Student dashboard Routing done here */}
-                    <Route path="/student-dashboard" element={<Student_App/>} >
-                        <Route index element={<Student_Main/>}/>
-                        <Route path="assisgnment" element={<FileUpload/>}/>
-                        <Route path="contactus" element={<ContactUs/>}/>
-                        <Route path="calendar" element={<Calendar/>}/>
-                    </Route>
-
-                </Routes>
-            </BrowserRouter>
+          {/* Student dashboard Routing done here */}
+          <Route path="/student-dashboard" element={<Student_App />}>
+            <Route index element={<Student_Main />} />
+            <Route path="assisgnment" element={<FileUpload />} />
+            <Route path="contactus" element={<ContactUs />} />
+            <Route path="calendar" element={<Calendar />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default Router;
--- src/Starting-Pages/Login.jsx
+++ src/Starting-Pages/Login.jsx
@@ -1,121 +1,121 @@
 import { useState } from 'react';
-import google from '../assets/google.svg'
+import google from '../assets/google.svg';
 import { Link } from 'react-router-dom';
 
-
-const cover_img = "https://plus.unsplash.com/premium_photo-1681505336207-cea25b6cf0ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2047&q=80"
-
-
-
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [emailError, setEmailError] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [emailError, setEmailError] = useState('');
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
 
-    const validateEmail = (email) => {
-        const re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    };
+  const clickhandler = (e) => {
+    e.preventDefault();
 
-    const clickhandler = (e) => {
-        e.preventDefault();
+    if (!validateEmail(email)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
 
-        if (!validateEmail(email)) {
-            setEmailError('Please enter a valid email address.');
-            return;
-        }
+    setEmailError('');
+    setEmail('');
+    setPwd('');
 
-        setEmailError('');
-        setEmail('');
-        setPwd('');
-
-        // Continue with your login logic
-    };
-
+    // Continue with your login logic
+  };
 
   return (
     <div className='w-full h-screen flex items-start'>
-        <div className='relative w-1/2 h-full flex flex-col'>
-            <div className='absolute top-[20%] left-[10%] flex flex-col'>
-                <h1 className='text-4xl text-white font-bold my-4'>Turn Your Ideas into reality</h1>
-                <p className='text-xl text-white font-normal'>Start for free and get attractive offers from the community</p>
-            </div>
-            <img src={cover_img} className='w-full h-full object-cover' />
+      <div className='relative w-1/2 h-full flex flex-col'>
+        <div className='absolute top-[20%] left-[10%] flex flex-col prose text-[#f5f5f5] z-10'>
+          <h1 className='text-5xl text-white font-extrabold my-4 font-poppins'>Turn Your Ideas into Reality</h1>
+          <p className='text-xl text-white font-normal font-poppins'>
+            Start for free and get attractive offers from the community
+          </p>
         </div>
+        <img
+          src="/images/vasily-koloda-8CqDvPuo_kI-unsplash.jpg"
+          className='w-full h-full object-cover brightness-75'
+        />
+      </div>
 
-        <div className='w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between items-center'>
-            <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto'>Interactive Brand</h1>
+      <div className='w-1/2 h-full bg-[#090909] flex flex-col p-20 justify-between items-center'>
+        <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#f5f5f5] font-semibold mr-auto'>
+          Interactive Brand
+        </h1>
 
-            <div className='w-full flex flex-col max-w-[500px]'>
-                <div className='w-full flex flex-col mb-2'>
-                    <h3 className='text-3xl font-semibold mb-2'>Login</h3>
-                    <p className='text-base mb-2'>Welcome Back! Please enter your details.</p>
-                </div>
+        <div className='w-full flex flex-col max-w-[500px]'>
+          <div className='w-full flex flex-col mb-2 text-white'>
+            <h3 className='text-3xl font-semibold mb-2'>Login</h3>
+            <p className='text-base mb-2 font-poppins'>Welcome Back! Please enter your details.</p>
+          </div>
 
-                <div className='w-full flex flex-col'>
-                    <input
-                        type='email'
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        placeholder='Email Address'
-                        className='w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none'
-                    />
+          <div className='w-full flex flex-col'>
+            <input
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email Address'
+              className='w-full text-[#f5f5f5] py-2 my-2 bg-transparent border-b border-[#f5f5f5] outline-none focus:outline-none'
+            />
 
-                    {emailError && <p className="text-red-600">{emailError}</p>}
+            {emailError && <p className='text-red-600'>{emailError}</p>}
 
-                    <input
-                        type='password'
-                        value={pwd}
-                        onChange={e => setPwd(e.target.value)}
-                        placeholder='Password'
-                        className='w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none'
-                    />
+            <input
+              type='password'
+              value={pwd}
+              onChange={(e) => setPwd(e.target.value)}
+              placeholder='Password'
+              className='w-full text-[#f5f5f5] py-2 my-2 bg-transparent border-b border-[#f5f5f5] outline-none focus:outline-none'
+            />
+          </div>
 
-                </div>
+          <div className='w-full flex items-center justify-between'>
+            <div className='w-full flex items-center'>
+              <input type='checkbox' className='w-4 h-4 mr-2' />
+              <p className='text-sm text-[#f5f5f5]'>Remember Me</p>
+            </div>
+            <a className='text-sm font-medium whitespace-nowrap cursor-pointer text-[#f5f5f5] underline underline-offset-2'>
+              Forgot Password ?
+            </a>
+          </div>
 
-                <div className='w-full flex items-center justify-between'>
-                    <div className='w-full flex items-center'>
-                        <input
-                            type='checkbox'
-                            className='w-4 h-4 mr-2'
-                        />
-                        <p className='text-sm'>Remember Me</p>
-                    </div>
-                    <a className='text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2'>Forgot Password ?</a>
-                </div>
-
-                <div className='w-full flex flex-col my-4'>
-                    <button className='w-full text-white my-2 font-semibold bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
-                    onClick={clickhandler}>
-                        Login
-                    </button>
-                    <button className='w-full text-[#060606] my-2 font-semibold bg-white border border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
+          <div className='w-full flex flex-col my-4'>
+            <button
+              className='w-full text-[#f5f5f5] my-2 font-semibold bg-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
+              onClick={clickhandler}
+            >
+              Login
+            </button>
+            {/* <button className='w-full text-[#060606] my-2 font-semibold bg-white border border-[#f5f5f5] rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
                         Register
-                    </button>
-                </div>
+                    </button> */}
+          </div>
 
-                <div className='w-full flex items-center justify-center relative py-2'>
-                    <div className='w-full h-[1px] bg-black/40'></div>
-                    <p className='text-lg absolute text-black/80 bg-[#f5f5f5]'>or</p>
-                </div>
+          <div className='w-full flex items-center justify-center relative py-2'>
+            <div className='w-full h-[1px] bg-[#f5f5f5]'></div>
+            <p className='text-lg absolute text-[#f5f5f5] bg-[#090909]'>or</p>
+          </div>
 
-                <div className='w-full text-[#060606] my-2 font-semibold bg-white border border-black/40 rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
-                    <img src={google} className='h-6 mr-2'/>
-                    Sign In with Google
-                </div>
-
-            </div>
-            <div className='w-full flex items-center justify-center'>
-                <p className='text-sm font-normal text-black'>Dont Have a account? 
-                   <Link to="/register">
-                      <span className='font-semibold underline underline-offset-2 cursor-pointer'>Sign up for free</span>
-                   </Link>
-                </p>
-            </div>
+          <div className='w-full text-[#f5f5f5] my-2 font-semibold bg-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
+            <img src={google} className='h-6 mr-2' />
+            Sign In with Google
+          </div>
         </div>
+        <div className='w-full flex items-center justify-center'>
+          <p className='text-sm font-normal text-[#f5f5f5]'>
+            Don&apos;t Have an Account?{' '}
+            <Link to='/register'>
+              <span className='font-semibold underline underline-offset-2 cursor-pointer'>Sign up for free</span>
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
